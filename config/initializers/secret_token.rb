3041bd9bbb3fc1a33cde74e49d1fb6094b41ba23
SG <sy1840@gmail.com>
2013-10-11 09:09:22 -0700

    Railscast on bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'e0f45a4c20061bef0d735771d3ecc07847494a24bf2cd75b64cc8e57838f9bd519d6cb3f7ca0736baf4d2c52c4fe63280dc0058c274198ad850dba3d414e1af3'
